b0710507b1eaec2163fe209116d11a9ea883513d
Bogdan Teteriuk <exxinblood@gmail.com>
2024-12-18 19:50:45 +0200

    back work

---
+++ src/generics/4.ts
@@ -0,0 +1,18 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+function createOrUpdateUser(initialValues: Partial<User>) {
+  // Оновлення користувача
+  console.log('User updated with values:', initialValues);
+}
+
+createOrUpdateUser({ 
+  email: 'user@mail.com', 
+  password: 'password123' 
+});
+
+    
