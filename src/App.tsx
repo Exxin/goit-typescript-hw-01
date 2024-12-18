d7b841ea34f28b4bc9580c84f30837b36d979293
Bogdan Teteriuk <exxinblood@gmail.com>
2024-12-18 19:48:15 +0200

    init rework on ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { fetchPhotos } from "./gallery-api";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import SearchBar from "./components/SearchBar/SearchBar";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import Loader from "./components/Loader/Loader";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import ImageModal from "./components/ImageModal/ImageModal";
+// import SearchBtn from "./components/SearchButton/SearchBtn";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleSearch = async (newQuery) => {
+    setQuery(newQuery);
+    setPage(1);
+    setImages([]);
+  };
+
+  const handleLoadMore = () => {
+    setPage(page + 1);
+  };
+
+  const openModal = (image) => {
+    setSelectedImage(image.urls.regular);
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    async function getPhotos() {
+      try {
+        setError(false);
+        setIsLoading(true);
+        const data = await fetchPhotos(query, page);
+        setImages((prevImages) => [...prevImages, ...data]);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getPhotos();
+  }, [page, query]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SearchBar onSearch={handleSearch} />
+      {/* <SearchBtn onSearchBtn={handleSearch}/> */}
+
+      {images.length > 0 && (
+        <ImageGallery items={images} onImageClick={openModal} />
+      )}
+
+      <ErrorMessage error={error} />
+
+      <Loader isLoading={isLoading} />
+
+      {images.length > 0 && (
+        <LoadMoreBtn isLoadMore={true} onClick={handleLoadMore} />
+      )}
+
+      <ImageModal
+        imageUrl={selectedImage}
+        isOpen={modalIsOpen}
+        onClose={closeModal}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
